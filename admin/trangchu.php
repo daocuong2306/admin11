cb69f23ee3a2d49de332a0d806b43f7047e07f6e
daocuong2306 <115203956+daocuong2306@users.noreply.github.com>
2022-12-07 12:35:06 +0700

    giaodien

---
+++ admin/trangchu.php
@@ -0,0 +1,191 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link rel="stylesheet" href="../view/css.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"
+          integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w=="
+          crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+
+</head>
+
+<body>
+<div class="tong">
+    <header>
+        <label for="check"></label>
+        <input type="checkbox" hidden name="" id="check">
+        <div class="menu-mobile">
+            <div class="onmjnf">
+                <label for="check" class="hi">
+                    <!-- <i class="fa-solid fa-bars"></i> -->
+                </label></div>
+
+            <div class="mau">
+
+                <div class="cutxeo">
+                    <img src="./img/4e30084eeb412b1f7250.jpg" alt="">
+                </div>
+                <p>Helo,..............</p>
+                <div class="menu">
+                    <div class="img_text">
+                        <a href="./admintrangchu.html"><i class="fa-solid fa-house-user"></i></a>
+                        <a href="./admintrangchu.html" id="abc">Trang chủ</a>
+                    </div>
+                    <div class="img_text">
+                        <a href="./indexcoso.html"> <i class="fa-solid fa-table"></i> </a>
+                        <a href="./indexcoso.html" id="abc">Cơ Sở</a>
+                    </div>
+                    <div class="img_text">
+                        <a href="./indexhocsinh.html"> <i class="fa-solid fa-user"></i></a>
+                        <a href="./indexhocsinh.html" id="abc">Học Sinh</a>
+                    </div>
+                    <div class="img_text">
+                        <a href="./indexgiaovien.html"><i class="fa-solid fa-book-open-reader"></i></a>
+                        <a href="./indexgiaovien.html" id="abc">Giao Viên</a>
+                    </div>
+                    <div class="img_text">
+                        <a href="./indexkhoahoc.html"><i class="fa-solid fa-book"></i></a>
+                        <a href="./indexkhoahoc.html" id="abc">Khóa Học</a>
+                    </div>
+                    <div class="img_text">
+                        <a href="./indexlichhoc.html"><i class="fa-solid fa-id-card"></i></a>
+                        <a href="./indexlichhoc.html" id="abc">lich hoc</a>
+
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </header>
+    <div class="cotenner">
+        <label for="check" class="hi">
+            <img src="./img/Vector.png" alt="">
+        </label>
+        <div class="xinchao">
+            <p>xin chào ........</p>
+        </div>
+        <div class="kkk">
+            <h3>Trang Chủ</h3>
+
+            <i class="fa-solid fa-house"></i>
+        </div>
+        <div class="conten">
+            <div class="chu">
+                <div class="text">
+                    <div class="noidung">
+                        <h4>Thông Tin Học Tập</h4>
+                    </div>
+                    <div class="the1">
+                        <i class="fa-solid fa-medal"></i>
+                        <a href="">Thông báo về kế hoạch thi tiếng anh kì fall 2022</a>
+                    </div>
+                    <div class="the1"> <i class="fa-solid fa-medal"></i>
+                        <a href="">Thông báo về việc học lại kì spring 2023</a>
+                    </div>
+                    <div class="the"> <a href="">Thông báo về đang kí chuyển ngành chuyên ngành chuyên ngành hẹp và
+                            chuyên cơ sở học kì
+                            -SPRING 2023</a></div>
+                    <div class="the"> <a href="">Thông báo về vị trí thực tập sinh ngành sàn thương mại điện tử</a>
+                    </div>
+                </div>
+                <div class="text">
+                    <div class="noidung">
+                        <h4>Thông Tin Hoạt động</h4>
+                    </div>
+                    <div class="the1"><i class="fa-solid fa-medal"></i> <a href="">Thông báo về triển khai BHYT nam
+                            2003</a></div>
+                    <div class="the"> <a href="">[GDQP]hông báo về việc học GDQP-AN tháng 8 năm 2022</a>
+                    </div>
+                    <div class="th">
+                        <a href="">Xem Thêm</a>
+                    </div>
+
+
+                </div>
+                <div class="text">
+                    <div class="noidung">
+                        <h4>Thông Tin Học Phí</h4>
+                    </div>
+                    <div class="the1"><i class="fa-solid fa-medal"></i>
+                        <a href="">Thông báo về hoc phí áp dụng từ kì Fall 2022</a>
+                    </div>
+                    <div class="th">
+                        <a href="">Xem Thêm</a>
+                    </div>
+                </div>
+                <div class="text">
+                    <div class="noidung">
+                        <h4>Thông Tin Việc Làm</h4>
+                    </div>
+
+                    <div class="the1"><i class="fa-solid fa-medal"></i><a href="">Thông báo về tuyển dụng CTV tư vấn
+                            tuyển sinh -Học Viện Công Nghệ </a></div>
+                    <div class="the1"><i class="fa-solid fa-medal"></i> <a href="">Thông báo về tuyển dụng nhân viên
+                            marketing</a></div>
+                    <div class="the"> <a href="">Thông báo về tuyển nhân viên chạy quảng cáo
+                        </a></div>
+                    <div class="the"> <a href="">Thông báo về tuyển dụng nhân viên sale </a></div>
+
+
+                </div>
+
+            </div>
+
+        </div>
+        <footer>
+            <div class="hihi">
+                <div class="anh">
+                    <img src="./img/image 10.png" alt="">
+                </div>
+                <div class="alo"> <i class="fa-solid fa-location-dot"></i>
+                    <p>Tòa nhà FPT Polytechnic, Phố Trịnh Văn Bô, Nam Từ Liêm, Hà Nội.</p></div>
+
+
+
+            </div>
+            <div class="hihi">
+                <h4>Thông tin liên hệ</h4>
+                <div class="lienhe">
+                    <i class="fa-solid fa-phone"></i>
+                    <p> Số điện thoại liên hệ giải đáp ý kiến sinh viên: 1900996686</p>
+                </div>
+                <div class="lienhe"><i class="fa-regular fa-envelope"></i>
+                    <p>Địa chỉ email các phòng ban:</p></div>
+
+
+                <ul><li>Phòng dịch vụ sinh viên: dvsvpoly.hn@poly.edu.vn</li>
+                    <li>Phòng Tổ chức và quản lý đào tạo :</li>
+                    <div class="dich"><li>Đào tạo: daotaopoly.hn@fe.edu.vn
+                        </li>
+                        <li>Khảo thí: khaothipolyhn@fe.edu.vn</li></div>
+
+                    <li>Phòng hành chính: hanhchinhfplhn@fe.edu.vn</li>
+                    <li>Phòng quan hệ doanh nghiệp: qhdn.poly@fe.edu.vn</li>
+                </ul>
+                <p>Ý kiến đóng góp chung gửi về ykien.poly@fpt.edu.vn bằng email @fpt.edu.vn</p>
+            </div>
+
+
+        </footer>
+
+
+
+
+    </div>
+</div>
+
+
+</div>
+
+
+</body>
+
+</html>
--- view/home.php
+++ view/home.php
@@ -5,22 +5,329 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="./css/admi.css">
+    <title>Trường học của những thiên tài !</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"
+        integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <link rel="stylesheet" href="./css/trangchinh.css">
     <script src="https://code.jquery.com/jquery-3.6.1.js"></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer"
-    />
+
 
 </head>
 
 <body>
-<div class="contenner">
-    <a href="index.php?act=giaovien">Giao vien</a>
-    <a href="index.php?act=hocsinh">hoc sinh</a>
-    <a href="index.php?act=admin">admin</a>
-</div>
-</body>
 
-</script>
+    <div id="contentner">
+        <div id="header">
+            <a href="" id="logo">
+                <img src="./img/image 10.png" alt="">
+            </a>
+            <ul id="main-menu">
+                <li><a href="index.php?act=giaovien">GIÁO VIÊN </a></li>
+                <li><a href="index.php?act=hocsinh">HỌC SINH </a></li>
+                <li><a href="">QUẢN TRỊ </a></li>
+                
+            </ul>               
+            <!-- <form action="" id="search-box">
+                <input type="text" id="search-text">
+                <button id="search-btn"><i class="fa-solid fa-magnifying-glass"></i></button>
+            </form> -->
+       
+        </div>
+        <div id="benner"><img src="./img/hinh-anh-san-truong-mua-hoa-phuong-1.jpg" alt=""></div>
+        <div id="conten">
+            <div id="contenn">
+                <h3>Chọn một lĩnh vực chủ đề :</h3>
+                <div class="text">
+                    <div class="chu">
+                        <p>CÔNG NGHỆ THÔNG TIN</p>
+                        <p>QUẢN TRỊ KINH DOANH</p>
+                    </div>
+                    <div class="chu">
+                        <p>THIẾT KẾ ĐỒ HỌA</p>
+                        <p>HƯỚNG DẪN DU LỊCH</p>
+                    </div>
+                    <div class="chu">
+                        <p>CÔNG NGHỆ CƠ KHÍ</p>
+                        <p>THẨM MỸ LÀM ĐẸP </p>
+                    </div>
+                </div>
+                <h3>CÁC KHÓA HỌC NỔI BẬT</h3>
+                <div class="cackhoahoc">
+                    <div class="khoahoc">
+
+                        <div class="anhchu">
+                            <div class="anh">
+                                <img src="./img/Screenshot 2022-11-07 021124.png" alt="">
+                            </div>
+                            <div class="chuuu">
+                                <h4>CÔNG NGHỆ THÔNG TIN</h4>
+                                <p>Giao sư tuấn anh</p>
+                                <h5>Ngày nay, điện thoại và các thiết bị thông minh đã trở nên phổ biến và dần chiếm ưu
+                                    thế
+                                    hơn
+                                    máy tính truyền thống. Trên 5 tỷ thiết bị lớn nhỏ từ các trung tâm dữ liệu, tài
+                                    chính
+                                    ngân hà
+                                    ng đều được lập trình bằng ngôn ngữ JAVA. Vì vậy những chuyên gia thành thạo ngôn
+                                    ngữ
+                                    JAVA luôn nhận được đãi ngộ rất tốt từ doanh nghiệp, công ty phát triển phần mềm,
+                                    cũng
+                                    như cơ
+                                    hội thăng tiến rộng mở.</h5>
+                                <p>7ki,5-7 giờ/tuần</p>
+                                <P>$1750</P>
+                            </div>
+
+                        </div>
+
+                    </div>
+                    <div class="khoahoc">
+
+                        <div class="anhchu">
+                            <div class="anh">
+                                <img src="./img/Screenshot 2022-11-07 021153.png" alt="">
+                            </div>
+                            <div class="chuuu">
+                                <h4>CÔNG NGHỆ THÔNG TIN</h4>
+                                <p>Giao sư tuấn anh</p>
+                                <h5>Ngày nay, điện thoại và các thiết bị thông minh đã trở nên phổ biến và dần chiếm ưu
+                                    thế
+                                    hơn
+                                    máy tính truyền thống. Trên 5 tỷ thiết bị lớn nhỏ từ các trung tâm dữ liệu, tài
+                                    chính
+                                    ngân hà
+                                    ng đều được lập trình bằng ngôn ngữ JAVA. Vì vậy những chuyên gia thành thạo ngôn
+                                    ngữ
+                                    JAVA luôn nhận được đãi ngộ rất tốt từ doanh nghiệp, công ty phát triển phần mềm,
+                                    cũng
+                                    như cơ
+                                    hội thăng tiến rộng mở.</h5>
+                                <p>7ki,5-7 giờ/tuần</p>
+                                <P>$1750</P>
+                            </div>
+
+                        </div>
+
+                    </div>
+                    <div class="khoahoc">
+
+                        <div class="anhchu">
+                            <div class="anh">
+                                <img src="./img/Screenshot 2022-11-07 021215.png" alt="">
+                            </div>
+                            <div class="chuuu">
+                                <h4>CÔNG NGHỆ THÔNG TIN</h4>
+                                <p>Giao sư tuấn anh</p>
+                                <h5>Ngày nay, điện thoại và các thiết bị thông minh đã trở nên phổ biến và dần chiếm ưu
+                                    thế
+                                    hơn
+                                    máy tính truyền thống. Trên 5 tỷ thiết bị lớn nhỏ từ các trung tâm dữ liệu, tài
+                                    chính
+                                    ngân hà
+                                    ng đều được lập trình bằng ngôn ngữ JAVA. Vì vậy những chuyên gia thành thạo ngôn
+                                    ngữ
+                                    JAVA luôn nhận được đãi ngộ rất tốt từ doanh nghiệp, công ty phát triển phần mềm,
+                                    cũng
+                                    như cơ
+                                    hội thăng tiến rộng mở.</h5>
+                                <p>7ki,5-7 giờ/tuần</p>
+                                <P>$1750</P>
+                            </div>
+                        </div>
+
+                    </div>
+
+                </div>
+                <div class="lolo">
+                    <a href="">KHÁM PHÁ TẤT CẢ KHÓA HỌC ></a>
+                </div>
+
+
+            </div>
+            <div class="concac">
+                <div class="delenne">
+                    <h3>ĐIỀU GÌ LÀM NÊN SỰ KHÁC BIỆT CỦA HBS ONLINE?
+                    </h3>
+                    <p>Các chương trình trực tuyến, linh hoạt của chúng tôi được thiết kế để mang lớp học của Trường
+                        Kinh doanh Harvard đến với bạn và được xây dựng dựa trên ba đặc điểm chính:</p>
+                    <div class="cotdelen">
+                        <div class="ban">
+                            <i class="fa-solid fa-gears"></i>
+
+                            <h4>Tích cực</h4>
+                            <p>Các khóa học trực tuyến của HBS không giống như một bài giảng ngồi lại và lắng nghe thông
+                                thường. Bạn sẽ tham gia vào một hoạt động mới cứ sau ba đến năm phút. Mỗi yếu tố được
+                                thiết kế để giữ cho bạn quan tâm, tham gia và trên đầu ngón chân của bạn</p>
+
+                        </div>
+                        <div class="ban">
+                            <i class="fa-solid fa-person-rifle"></i>
+
+                            <h4>Tích cực</h4>
+
+                            <p>Tương tác với các đồng nghiệp của bạn là một phần quan trọng tạo nên sự độc đáo của HBS
+                                Online. Bạn trao đổi ý kiến, cung cấp thông tin đầu vào và tìm kiếm quan điểm từ một
+                                cộng đồng người học được tập hợp từ khắp nơi trên thế giới. Trong quá trình này, bạn học
+                                hỏi kinh nghiệm và quan điểm của nhau.</p>
+
+                        </div>
+                        <div class="ban">
+                            <i class="fa-solid fa-book"></i>
+
+                            <h4>Tích cực</h4>
+                            <p>Đắm chìm trong học tập trong thế giới thực. Các bài học được đưa vào cuộc sống thông qua
+                                các trường hợp đặt bạn vào vị trí của các nhà lãnh đạo doanh nghiệp. Tận mắt nghe những
+                                thách thức mà họ gặp phải, và vật lộn với những vấn đề tương tự và thông tin không hoàn
+                                hảo.</p>
+
+                        </div>
+
+
+                    </div>
+                    <div class="pl">
+                        <a href=""> TÌM HIỂU THÊM VỀ MÔ HÌNH HỌC TẬP TRỰC TUYẾN CỦA HVCN >
+                        </a>
+
+                    </div>
+                </div>
+
+
+            </div>
+            <div class="llll">
+                <img src="./img/nhung-hinh-anh-ve-mai-truong-xua-hoai-niem-dep-nhat-820x461.jpg" alt="">
+
+            </div>
+          
+            <div class="on">
+                <h2>Cần thêm thông tin? </h2>
+                <p>Nếu bạn đang suy nghĩ về cách chuẩn bị cho giai đoạn tiếp theo trong sự nghiệp của mình, chúng tôi có
+                    thể giúp bạn. Yêu</br> cầu thêm thông tin ngày hôm nay</p>
+                <a href="">YÊU CẦU THÊM THÔNG TIN ></a>
+            </div>
+            <div class="mlk">
+                <div class="gancuoi">
+                    <div class="borderduoi">
+                        <h3>Câu hỏi thường gặp hàng đầu : </h3>
+                        
+                    </div>
+                    <div class="borderduoi">    
+                        <p>Trường còn tuyển sinh không?</p>
+                        <p>+</p>
+                    </div>
+                    <div class="borderduoi">
+                        <p>các khóa học trực tuyến của trường đc cung cấp như thế nào?</p>
+                        <p>+</p>
+                    </div>
+                    <div class="borderduoi">
+                        <p>Tôi có cần đến trường để tham gia các chương trình trực tuyến  không?</p>
+                        <p>+</p>
+                    </div>
+                    <div class="borderduoi">
+                        <p>Làm thế nào để tôi ghi danh vào một khóa học?</p>
+                        <p>+</p>
+                    </div>
+                    <div class="borderduoi">
+                        <p>Lựa chọn thanh toán của tôi là gì?</p>
+                        <p>+</p>
+                    </div>
+                   
+                </div>
+                <div class="lololol">
+                    <h3>Đăng ký nhận tin tức & thông báo</h3>
+                    <p>Thư Điện tử *</p>
+                    <form action="">
+                        <input type="text" placeholder="">
+                    </form>
+                    <p>Sử dụng công nghệ hỗ trợ</p>
+                    <button>Nôp</button>
+                    <div class="cott4">
+                        <div class="cotne">
+                            <h3>Các môm học</h3>
+                            <p>Yếu Tố Kinh Doanh
+                               </p>
+                            <p>Lãnh đạo & Quản lý</p>
+                            <p> Khởi nghiệp & Đổi mới</p>
+                            <p> Chiến lược</p>
+                            <p>Tài chình kế toán</p>
+                        </div>
+
+                        <div class="cotne">
+                            <h3>Các môm học</h3>
+                            <p>Yếu Tố Kinh Doanh
+                               </p>
+                            <p>Lãnh đạo & Quản lý</p>
+                            <p> Khởi nghiệp & Đổi mới</p>
+                            <p> Chiến lược</p>
+                            <p>Tài chình kế toán</p>
+                        </div>
+                        <div class="cotne">
+                            <h3>Các môm học</h3>
+                            <p>Yếu Tố Kinh Doanh
+                               </p>
+                            <p>Lãnh đạo & Quản lý</p>
+                            <p> Khởi nghiệp & Đổi mới</p>
+                            <p> Chiến lược</p>
+                            <p>Tài chình kế toán</p>
+                        </div>
+                        <div class="cotne">
+                            <h3>Các môm học</h3>
+                            <p>Yếu Tố Kinh Doanh
+                               </p>
+                            <p>Lãnh đạo & Quản lý</p>
+                            <p> Khởi nghiệp & Đổi mới</p>
+                            <p> Chiến lược</p>
+                            <p>Tài chình kế toán</p>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+        
+
+        <footer>
+            <div class="hihi">
+                <div class="anh">
+                    <img src="./img/image 10.png" alt="">
+                </div>
+                <div class="alo"> <i class="fa-solid fa-location-dot"></i>
+                    <p>Tòa nhà FPT Polytechnic, Phố Trịnh Văn Bô, Nam Từ Liêm, Hà Nội.</p></div>
+                    
+               
+
+            </div>
+            <div class="hihi">
+                <h4>Thông tin liên hệ</h4>
+                <div class="lienhe">
+                    <i class="fa-solid fa-phone"></i>
+                    <p> Số điện thoại liên hệ giải đáp ý kiến sinh viên: 1900996686</p>
+                </div>
+                <div class="lienhe"><i class="fa-regular fa-envelope"></i>
+                    <p>Địa chỉ email các phòng ban:</p></div>
+               
+                
+                <ul><li>Phòng dịch vụ sinh viên: dvsvpoly.hn@poly.edu.vn</li>
+                    <li>Phòng Tổ chức và quản lý đào tạo :</li>
+                    <div class="dich"><li>Đào tạo: daotaopoly.hn@fe.edu.vn
+                        </li>
+                        <li>Khảo thí: khaothipolyhn@fe.edu.vn</li></div>
+                    
+                    <li>Phòng hành chính: hanhchinhfplhn@fe.edu.vn</li>
+                    <li>Phòng quan hệ doanh nghiệp: qhdn.poly@fe.edu.vn</li>
+                </ul>
+                <p>Ý kiến đóng góp chung gửi về ykien.poly@fpt.edu.vn bằng email @fpt.edu.vn</p>
+            </div>
+
+
+        </footer>
+    </div>
+
+
+    </div>
+
+</body>
 
 </html>
